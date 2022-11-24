d11b89602182ea2b0c3e8a41b82e758355c5fa34
Emma Nielsen <Emmanielsen843@gmail.com>
2022-11-24 12:14:15 +0100

    idk anymore

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,11 +1,25 @@
-async function getData() {
-	try {
-		const response = await fetch('http://songbook.local/api/song/7')
-		const data = await response.json();
-		const { artist_id, id, title, content } = data;
-		console.log(data);
-	} catch (err) {
-		alert(err)
-	}
-}
-getData();
+// async function getData() {
+// 	try {
+// 		const response = await fetch('http://songbook.local/api/song/7')
+// 		const data = await response.json();
+// 		const { artist_id, id, title, content } = data;
+// 		console.log(data);
+// 	} catch (err) {
+// 		alert(err)
+// 	}
+// }
+// getData();
+
+let apiData = [];
+
+fetch('http://phpopgave.local/api/song/')
+
+.then(response => 
+	response.json()
+)
+.then(data =>
+	console.log(data)
+)
+.catch((error) => {
+	console.error(error);
+})
